ea4cd58657b785b26eb6f9a99ce18cc9b4c13c24
Edgar Campos <Camposedgar899@gmail.com>
2024-03-04 10:14:33 -0600

    Creacion de proyecto e inicio de configuracion para producto

---
+++ Domain/Productos/Producto.cs
@@ -0,0 +1,29 @@
+using Domain.Primitives;
+
+namespace Domain.Productos;
+
+public sealed class Producto : AggregateRoot
+{
+    public Producto(ProductoId id, string nombre, string descripcion, double precio)
+    {
+        Id = id;
+        Nombre = nombre;
+        Descripcion = descripcion;
+        Precio = precio;
+    }
+
+    private Producto()
+    {
+
+    }
+
+    public ProductoId Id { get; private set; }
+    public string Nombre { get; private set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+    public double Precio { get; set; } = double;
+
+    public static Producto UpdateProducto(Guid id, string nombre, string descripcion, double precio)
+    {
+        return new Producto(new ProductoId(id), nombre, descripcion, precio);
+    }
+}
